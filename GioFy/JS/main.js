a8bab88bd1bf080a4559e487a7e5a91c10fa7459
GioDevJS <sgtech@live.com>
2024-06-14 14:07:33 -0300

    blocks, navBar

---
+++ GioFy/JS/main.js
@@ -0,0 +1,9 @@
+const collapsibles = document.querySelectorAll('.collapsible');
+
+collapsibles.forEach((item) => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('collapsible--expanded');
+    });
+});
+
+
